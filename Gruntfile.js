8113ee76f702cb035ddfcbf70bb35d719002fcaf
Davide Laezza <truzzialrogo@gmx.com>
2016-05-01 18:57:51 +0200

    Added autoprefixer and uglify to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,12 @@ module.exports = function (grunt) {
 			options: {
 				banner: '(function(angular) {\n\n',
 				footer: ';\n\n})(angular);',
-				process: function(src, filePath) {
+
+				/*
+					Removes IIFE and AngularJS module retrieval,
+					to create an unique chained module declaration
+				*/
+				process: function(src) {
 					return src.replace(/\(function\(.*\)\s*{\s*/, '')
 							.replace(/;?\s*\}\)\(.*\);\s*$/g, '')
 							.replace(/angular.module\(["']dragonSearch["']\)\n/g, '');
@@ -33,13 +38,50 @@ module.exports = function (grunt) {
 		},
 
 		uglify: {
+			options: {
+				mangle: true,
+				compress: {
+					unsafe: true,
+					collapse_vars: true,
+					pure_getters: true,
+					keep_fargs: false,
+					angular: true
+				},
+				quoteStyle: 3
+			},
+
+			breed: {
+				src: 'js/breedingHints.js',
+				dest: 'js/breedingHints.js'
+			},
+
+			index: {
+				src: 'js/index.js',
+				dest: 'js/index.js'
+			}
+		},
+
+		postcss: {
+			options: {
+				map: false,
+				processors: [
+					require('autoprefixer')
+				]
+			},
+
+			breed: {
+				src: 'css/breedingHints.css',
+				dest: 'css/breedingHints.css'
+			}
 		}
 	});
 
+	// Load plugins that provide the "concat", "uglify" and "autoprefixer" tasks
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-postcss');
 
+	// Tasks
 	grunt.registerTask('index', ['concat:index'/*, 'uglify:index'*/]);
-
-	grunt.registerTask('breed', ['concat:breed'/*, 'uglify:breed'*/]);
+	grunt.registerTask('breed', ['concat:breed', 'uglify:breed', 'postcss:breed']);
 };
