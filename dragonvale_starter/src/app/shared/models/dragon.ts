96b09a63ab6a5b8353bac776202c5f5678abe33d
Davide Laezza <truzzialrogo@gmx.com>
2016-10-10 20:32:52 +0200

    Commented Dragon interface

--- dragonvale_starter/src/app/shared/models/dragon.ts
+++ dragonvale_starter/src/app/shared/models/dragon.ts
@@ -1,11 +1,25 @@
 /**
  * Created by maze on 9/25/16.
+ *
+ * This interface models a dragon
+ * basic traits.
  */
 
 interface Dragon {
+
+    /** The dragon id, mainly used in Web API calls. */
     id: number;
+
+    /** The dragon name. */
     name: string;
+
+    /**
+     * The dragon hatching time, not reduced
+     * and without days displayed.
+     */
     time: string;
+
+    /** The dragon element names. */
     elems: string[];
 }
 
